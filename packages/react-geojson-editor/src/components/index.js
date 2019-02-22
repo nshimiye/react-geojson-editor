cc322db1d86bee302e90d53f612e57fe81d9f45c
Marcellin Nshimiyimana <marcellin2@gmail.com>
2019-02-22 10:51:47 -0500

    rearrange components

--- packages/react-geojson-editor/src/components/index.js
+++ packages/react-geojson-editor/src/components/index.js
@@ -1,13 +1,13 @@
-import GeoJsonEditorComponent from './GeoJsonEditor/index';
 import ButtonComponent from './Button/index';
 import DropdownComponent from './Dropdown/index';
+import GeoJsonEditorComponent from './GeoJsonEditor/index';
 
-export const GeoJsonEditor = GeoJsonEditorComponent;
-export const Dropdown = HelloWorldComponent;
 export const Button = HelloWorldComponent;
+export const Dropdown = HelloWorldComponent;
+export const GeoJsonEditor = GeoJsonEditorComponent;
 
 export default {
-  GeoJsonEditor: GeoJsonEditorComponent,
-  Dropdown: DropdownComponent,
   Button: ButtonComponent,
+  Dropdown: DropdownComponent,
+  GeoJsonEditor: GeoJsonEditorComponent,
 };
